921d9b47ca0115549519005d1ed477bd7c554270
pi <pi@openMHA>
2017-12-26 16:56:36 +0000

    Added example script to measure feedback, latency, and IO-functions.

---
+++ tools/record_MHAIO.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+PLAYFILE="$1"
+DURATION="$2"
+RECFILE="$3"
+
+if [ $# -lt 3 ]; then
+  echo "usage playrec.sh FILE DURATION"
+  exit 1
+fi
+
+if [ ! -e "$PLAYFILE" ]; then
+  echo "File '${PLAYFILE}' does not exist!"
+  exit 1
+fi
+
+echo "Start playback"
+mplayer -ao jack:name=mplayer:noestimate:port=MHA "${PLAYFILE}" &
+sleep 1
+
+
+echo "Start recording"
+jack_rec -f "${RECFILE}" -d "${DURATION}" -b 32 mplayer:out_0 mplayer:out_1 MHA:out_1 MHA:out_2
+
+
+
