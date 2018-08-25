fb1579a9a4b6ac50dbed0ac32442af56839bc69d
pi <pi@openMHA>
2018-08-25 21:01:37 +0000

    Fix compilation

--- make.sh
+++ make.sh
@@ -9,7 +9,6 @@ error() {
 
 # Compile all C code
 
-(cd tools/control && gcc ${CFLAGS} translate.c -o translate) || error "translate"
 (cd tools/abhang/src/jack && gcc ${CFLAGS} abhang.c -o abhang -lm -ljack) || error "abhang"
 (cd tools/signals && gcc ${CFLAGS} whitenoise.c -o whitenoise -lm -ljack) || error "whitenoise"
 (cd tools/signals && gcc ${CFLAGS} sweep.c -o sweep -lm -ljack) || error "sweep"
