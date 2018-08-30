ca72eb5d0a2c0018952e745709fd81b2bab75fae
Marc René Schädler <marc.r.schaedler@uni-oldenburg.de>
2018-08-30 14:27:39 +0200

    Add license and licensing information in file headers
    Also added a reference for the SIAM procedure

--- tools/abhang/src/core/variables.h
+++ tools/abhang/src/core/variables.h
@@ -1,3 +1,14 @@
+/**
+ * Copyright 2018 Marc René Schädler
+ *
+ * This file is part of the mobile hearing aid prototype project
+ * The the mobile hearing aid prototype project is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * The mobile hearing aid prototype project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with the mobile hearing aid prototype project. If not, see http://www.gnu.org/licenses/.
+ */
+
 float feedback1[FEEDBACKLENGTH*TICKSAMPLES] = {0.0};
 float feedback2[FEEDBACKLENGTH*TICKSAMPLES] = {0.0};
 float playbackbuffer1[FEEDBACKLENGTH*TICKSAMPLES] = {0.0};
--- tools/prescription_minimalistic.m
+++ tools/prescription_minimalistic.m
@@ -1,3 +1,12 @@
+% Copyright 2018 Marc René Schädler
+%
+% This file is part of the mobile hearing aid prototype project
+% The the mobile hearing aid prototype project is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+%
+% The mobile hearing aid prototype project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License along with the mobile hearing aid prototype project. If not, see http://www.gnu.org/licenses/.
+
 function [gt_data, gt_freqs, gt_levels] = prescription_minimalistic(freqs, thresholds_left, thresholds_right, offset, marginfactor, rolloff)
   gt_freqs = [250 500 1000 1500 2000 3000 4000 6000 8000];
   gt_levels = -10:1:110;
