a53ae59ca2593fc9fce94e6f2d0a1a6081a7d778
Marc René Schädler <marc.r.schaedler@uni-oldenburg.de>
2019-10-14 12:28:23 +0200

    Adapt Octave signal processing test script

--- playrec/play_test_processing.m
+++ playrec/play_test_processing.m
@@ -9,40 +9,31 @@ clear compress
 
 
 fs = 48000;
-sweep = real(exp(1i*2*pi*cumsum(linspace(10/fs,10000/fs,fs)))).';
 tone = sin(2*pi*linspace(0,1000,fs)).';
-noise = randn(fs,1);
-clicks = zeros(fs,1);
-clicks(1:100:fs) = 10.^(linspace(0,130,480)./20);
+toneamp = tone.*10.^(linspace(-97,3,fs).'./20);
 
-in = [tone tone];
+in = [tone toneamp];
 out = zeros(size(in));
 
-N = 192;
-FFTN = 256;
-M = N/4;
-calibration = 100 - 10*log10(FFTN*M/2);
-
-cfreqs = [75 125 250 500 1000 2000 4000 8000 16000]./fs;
-gaintablecomp = linspace(99,0,100).' .* ones(1,length(cfreqs)-2);
-gaintablenone = zeros(100,length(cfreqs)-2);
-gaintablezero = -inf(100,length(cfreqs)-2);
-gaintable = [gaintablecomp gaintablecomp];
+M = 128;
+N = 256;
+FFTN = 512;
 
 tic
-overlapadd(zeros(M,2),N,2);
-compress(zeros(N,2), FFTN, cfreqs, gaintable, calibration);
+process([], fs, M, N, FFTN);
 toc
 
+profile off;
 profile clear;
-
+profile on;
 for i=1:M:size(in,1)
   in_tmp = in((i:i+M-1),:);
   tic;
-  out_tmp = overlapadd(in_tmp);
+  out_tmp = process(in_tmp);
   printf('%.3f\n',toc./(M/fs));
   out(i:(i+M-1),:) = out_tmp;
 end
+profile off;
 
 figure;
 subplot(2,2,1);
