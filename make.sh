29d6095f3cbb4f203f1f9c27da43a7978ac541de
pi <pi@openMHA>
2021-05-24 12:46:52 +0000

    Compile with march=native

--- make.sh
+++ make.sh
@@ -9,7 +9,7 @@
 #
 # You should have received a copy of the GNU General Public License along with the mobile hearing aid prototype project. If not, see http://www.gnu.org/licenses/.
 
-CFLAGS="-Wall -Ofast"
+CFLAGS="-Wall -Ofast -march=native"
 
 error() {
   echo "$1"
